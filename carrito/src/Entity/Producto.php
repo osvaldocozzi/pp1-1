958d60c317e886fa128ae0e98afd6fd739560060
meka <meka__cozzi@hotmail.com>
2023-08-29 18:18:25 -0300

    commit base de datos

---
+++ carrito/src/Entity/Producto.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ProductoRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ProductoRepository::class)]
+class Producto
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 30)]
+    private ?string $nombre = null;
+
+    #[ORM\Column(length: 100)]
+    private ?string $Descripcion = null;
+
+    #[ORM\Column(length: 50)]
+    private ?string $Imagen = null;
+
+    #[ORM\Column]
+    private ?float $Precio = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNombre(): ?string
+    {
+        return $this->nombre;
+    }
+
+    public function setNombre(string $nombre): static
+    {
+        $this->nombre = $nombre;
+
+        return $this;
+    }
+
+    public function getDescripcion(): ?string
+    {
+        return $this->Descripcion;
+    }
+
+    public function setDescripcion(string $Descripcion): static
+    {
+        $this->Descripcion = $Descripcion;
+
+        return $this;
+    }
+
+    public function getImagen(): ?string
+    {
+        return $this->Imagen;
+    }
+
+    public function setImagen(string $Imagen): static
+    {
+        $this->Imagen = $Imagen;
+
+        return $this;
+    }
+
+    public function getPrecio(): ?float
+    {
+        return $this->Precio;
+    }
+
+    public function setPrecio(float $Precio): static
+    {
+        $this->Precio = $Precio;
+
+        return $this;
+    }
+}
